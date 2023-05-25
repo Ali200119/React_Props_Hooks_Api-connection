97d9f1bc186d47bcf587ea66414b021150ac538b
Ali200119 <aliit@code.edu.az>
2023-05-25 04:26:19 +0400

    feat: State

--- routing/src/pages/Home.jsx
+++ routing/src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Product from '../components/common/Product'
+import {Button} from "reactstrap"
+import { useState } from 'react';
 
 function Home() {
   const products = ["iPhone, Nokia, Samsung, Sony"];
 
 
-  
+  // let num = 3;
+
+  // function add() {
+  //   num++;
+  // }
+
+  const[num, setNum] = useState(3);
+
+  const add = () => {
+    setNum(num + 1)
+  }
+
   return (
     <main>
       <section id="product">
@@ -21,6 +34,10 @@ function Home() {
               })
             }
           </div>
+
+          <h1>{num}</h1>
+
+          <Button onClick={add}>Create</Button>
         </div>
       </section>
     </main>
